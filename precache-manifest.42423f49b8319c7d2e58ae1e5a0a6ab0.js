3d16ceb77c369a81e9f7f415e1eb88c7b3544345
Samin Sharar <saminsharar2@gmail.com>
2020-08-15 18:37:00 +0600

    Updates

---
+++ precache-manifest.42423f49b8319c7d2e58ae1e5a0a6ab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f333d4c8d06f5aa96aaf3d7ad756d1",
+    "url": "/BasicRoboFriends/index.html"
+  },
+  {
+    "revision": "a46309f9335418c1a044",
+    "url": "/BasicRoboFriends/static/css/main.cd2d8219.chunk.css"
+  },
+  {
+    "revision": "30d5af1b286eb67932d2",
+    "url": "/BasicRoboFriends/static/js/2.fc841e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BasicRoboFriends/static/js/2.fc841e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a46309f9335418c1a044",
+    "url": "/BasicRoboFriends/static/js/main.fcbebd93.chunk.js"
+  },
+  {
+    "revision": "7fefe4b2cd0e6f13a9a9",
+    "url": "/BasicRoboFriends/static/js/runtime-main.61d5a208.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/BasicRoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
